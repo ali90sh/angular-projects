835ad323f81bd7a98f9d3d1bcf8379902e652f37
Ali Shahgholi <ali.shahgholi90@gmail.com>
2025-11-05 10:39:37 +0330

    refactor(serverManagementApp): dashboard-items wrapper added

--- serverManagementApp/src/app/app.component.ts
+++ serverManagementApp/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+import { DashboardItemsComponent } from "./dashboard/dashboard-items/dashboard-items.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
